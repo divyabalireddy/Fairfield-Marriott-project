c5b70aa6338bfd6a2f3d85485f4a64fefe9923a5
divyabalireddy <161671824+divyabalireddy@users.noreply.github.com>
2024-06-11 16:20:06 +0530

    Update Room.java

--- OnlineHotelBooking/src/main/java/com/tmf/OnlineHotelBooking/OnlineHotelBooking/model/Room.java
+++ OnlineHotelBooking/src/main/java/com/tmf/OnlineHotelBooking/OnlineHotelBooking/model/Room.java
@@ -4,11 +4,32 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private long id;
+	
 	private String roomType;
 	private BigDecimal roomprice;
 	private boolean isBooked=false;
+	@OneToMany(fetch = FetchType.LAZY,cascade=CascadeType.ALL)
 	private List<BookedRoom> bookings;
 
 public Room() {
@@ -17,4 +38,14 @@ public Room() {
 	
 }	
 
+public void addBooking(BookedRoom booking){
+	if(bookings = null) {
+		bookings=new ArrayList<>();
+	}
+		
+	}
+}
+{
+	
+}
 }
