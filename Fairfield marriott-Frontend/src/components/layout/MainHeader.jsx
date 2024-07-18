e0751bc1c6b00a1c1f488b93ab2a04c1c1885f29
divyabalireddy <161671824+divyabalireddy@users.noreply.github.com>
2024-07-18 13:08:38 +0530

    Add files via upload

---
+++ Fairfield marriott-Frontend/src/components/layout/MainHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const MainHeader = () => {
+	return (
+		<header className="header-banner">
+			<div className="overlay"></div>
+			<div className="animated-texts overlay-content">
+				<h1>
+					Welcome to <span className="hotel-color"> Fairfield Marriott </span>
+				</h1>
+				<h4>Effortless Booking, Memorable Stays</h4>
+			</div>
+		</header>
+	)
+}
+
+export default MainHeader
